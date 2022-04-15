bd7a884646f2e43844b1a4ee1ea3c1391986f447
robertoandres24 <robertoandres24@gmail.com>
2022-04-14 23:03:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const watchlist = require('./api/watchlist')
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.use('/api/watchlist', watchlist)
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
