34f6f476b6f62d084a012aa881872abc17bd2508
roberto <macpro-roberto@MacBookPro.local>
2022-04-26 00:41:59 -0400

    try to fix Chromium not found on Vercel

--- api/imdbScrapper.js
+++ api/imdbScrapper.js
@@ -1,8 +1,26 @@
-const puppeteer = require('puppeteer');
+/* eslint-disable global-require */
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+} else {
+  // running locally.
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  puppeteer = require('puppeteer');
+}
 
 (async () => {
   const imdbUrl = 'https://www.imdb.com/user/ur60351403/watchlist';
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
+  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(imdbUrl);
   page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
