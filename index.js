64f12f8a04b0ab26b93a4c82dec3ff61ba6adea3
roberto <macpro-roberto@MacBookPro.local>
2022-04-27 18:42:15 -0400

    fix

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const imdbScrapper = require('./scrappers/imdbScrapper');
 const { imdbList } = require('./store');
 
 const app = express();
+const port = 3000;
 
 imdbScrapper.run()
   .then((list) => {
     imdbList.push(...list);
     console.log('Imdb List: ', imdbList);
-    const port = 3000;
-
-    app.get('/', (req, res) => {
-      res.send('Hello new World');
-    });
-
-    app.use('/api/watchlist', watchlist);
-
-    app.listen(port, () => {
-      console.log(`Example app listening on http://localhost:${port}`);
-    });
   })
   .catch((e) => console.log('Error Trying to get Imdb List: ', e));
 
-module.exports = app;
+app.get('/', (req, res) => {
+  res.send('Hello new World');
+});
+
+app.use('/api/watchlist', watchlist);
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
