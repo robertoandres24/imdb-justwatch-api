f383defe0fe499d1c98de23fe69043f5c4fc358c
roberto <macpro-roberto@MacBookPro.local>
2022-04-27 19:22:47 -0400

    test axios

--- api/watchlist.js
+++ api/watchlist.js
@@ -1,15 +1,21 @@
+const { default: axios } = require('axios');
 const express = require('express');
-const imdbScrapper = require('../scrappers/imdbScrapper');
+// const imdbScrapper = require('../scrappers/imdbScrapper');
 
 const router = express.Router();
 
+async function getTodo() {
+  return axios.get('https://jsonplaceholder.typicode.com/todos/1');
+}
+
 router.get('/', async (req, res) => {
   try {
+    const { data: todo } = await getTodo();
+    console.log(todo);
     return res.json({
       status: 200,
       message: 'Get data has successfully',
-      data: await imdbScrapper.run(),
-      foo: 'bar',
+      data: todo,
     });
   } catch (error) {
     console.error(error);
