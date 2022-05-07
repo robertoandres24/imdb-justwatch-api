6aa10f06ffa922bf9d1d65b1dbaf8fba824a4fde
roberto <robertoandres24@gmail.com>
2022-05-07 01:10:52 -0400

    fix es6 module, back to commonjs

--- index.js
+++ index.js
@@ -5,35 +5,30 @@ const dbConnect = require('./db/dbConnect');
 const ImdbItem = require('./db/models/ImdbItem');
 const { runImdbScrapper } = require('./scrappers/imdbScrapper');
 
-async function init() {
-  await dbConnect();
-  console.log('Database connected!');
-  console.log('process.env.NODE_ENV');
-  console.log(process.env.NODE_ENV);
-  console.log('Srapping ....');
-  const imdbItemIds = await runImdbScrapper();
-  console.log('🚀 ~ file: index.js ~ line 12 ~ .then ~ imdbItemIds', imdbItemIds);
-  const app = express();
-  const port = 3000;
+dbConnect()
+  .then(async () => {
+    console.log('Database connected!');
+    console.log('process.env.NODE_ENV');
+    console.log(process.env.NODE_ENV);
+    console.log('Srapping ....');
+    const imdbItemIds = await runImdbScrapper();
+    console.log('🚀 ~ file: index.js ~ line 12 ~ .then ~ imdbItemIds', imdbItemIds);
+    const app = express();
+    const port = 3000;
 
-  app.get('/', async (req, res) => {
-    const imdbItems = await (await ImdbItem.find()).map((item) => item.id);
-    res.send({
-      total: imdbItems.length,
-      imdbItems,
-      foo: process.env.foo || 'default',
+    app.get('/', async (req, res) => {
+      const imdbItems = await (await ImdbItem.find()).map((item) => item.id);
+      res.send({
+        total: imdbItems.length,
+        imdbItems,
+        foo: process.env.foo || 'default',
+      });
     });
-  });
 
-  app.use('/api/watchlist', watchlist);
+    app.use('/api/watchlist', watchlist);
 
-  app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
-  });
-}
-
-try {
-  init();
-} catch (error) {
-  throw new Error(error);
-}
+    app.listen(port, () => {
+      console.log(`Example app listening on http://localhost:${port}`);
+    });
+  })
+  .catch((e) => console.log(e));
