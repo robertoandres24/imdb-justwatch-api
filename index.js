1afee8c52d5a8e7fd13f5997a33102b7cfcc5591
roberto <robertoandres24@gmail.com>
2022-05-08 21:36:19 -0400

    test jsonplaceholder

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
+const { default: axios } = require('axios');
 const express = require('express');
-const watchlist = require('./api/watchlist');
+// const watchlist = require('./api/watchlist');
 const dbConnect = require('./db/dbConnect');
-const ImdbItem = require('./db/models/ImdbItem');
-const { runImdbScrapper } = require('./scrappers/imdbScrapper');
+// const ImdbItem = require('./db/models/ImdbItem');
+// const { runImdbScrapper } = require('./scrappers/imdbScrapper');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 dbConnect()
   .then(async () => {
@@ -14,18 +15,19 @@ dbConnect()
     console.log('process.env.NODE_ENV');
     console.log(process.env.NODE_ENV);
     console.log('Srapping ....');
-    const imdbItemIds = await runImdbScrapper();
-    console.log('🚀 ~ file: index.js ~ line 12 ~ .then ~ imdbItemIds', imdbItemIds);
+    // const imdbItemIds = await runImdbScrapper();
+    // console.log('🚀 ~ file: index.js ~ line 12 ~ .then ~ imdbItemIds', imdbItemIds);
     app.get('/', async (req, res) => {
-      const imdbItems = await (await ImdbItem.find()).map((item) => item.id);
+      // const imdbItems = await (await ImdbItem.find()).map((item) => item.id);
+      const { data: todo, status } = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
       res.send({
-        total: imdbItems.length,
-        imdbItems,
-        foo: process.env.foo || 'default',
+        hello: 'world',
+        todo,
+        status,
       });
     });
 
-    app.use('/api/watchlist', watchlist);
+    // app.use('/api/watchlist', watchlist);
 
     app.listen(port, () => {
       console.log(`Example app listening on http://localhost:${port}`);
