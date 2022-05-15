82966509756287ec1747d648bdcc890821ee849d
roberto <robertoandres24@gmail.com>
2022-05-14 20:02:06 -0400

    take puppeter back

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const { default: axios } = require('axios');
+
 const express = require('express');
-// const watchlist = require('./api/watchlist');
+const watchlist = require('./api/watchlist');
 const dbConnect = require('./db/dbConnect');
-// const ImdbItem = require('./db/models/ImdbItem');
-// const { runImdbScrapper } = require('./scrappers/imdbScrapper');
+
+const { runImdbScrapper } = require('./scrappers/imdbScrapper');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,22 +15,15 @@ dbConnect()
     console.log('process.env.NODE_ENV');
     console.log(process.env.NODE_ENV);
     console.log('Srapping ....');
-    const { data: todo } = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-    console.log('🚀 ~ file: index.js ~ line 19 ~ .then ~ todo', todo);
 
-    // const imdbItemIds = await runImdbScrapper();
-    // console.log('🚀 ~ file: index.js ~ line 12 ~ .then ~ imdbItemIds', imdbItemIds);
+    const list = await runImdbScrapper();
     app.get('/', async (req, res) => {
-      // const imdbItems = await (await ImdbItem.find()).map((item) => item.id);
-      const { data: theTodo, status } = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      res.send({
-        hello: 'world',
-        theTodo,
-        status,
+      res.json({
+        list,
       });
     });
 
-    // app.use('/api/watchlist', watchlist);
+    app.use('/api/watchlist', watchlist);
 
     app.listen(port, () => {
       console.log(`Example app listening on http://localhost:${port}`);
--- scrappers/imdbScrapper.js
+++ scrappers/imdbScrapper.js
@@ -1,45 +1,27 @@
-// in localhost use puppeteer, in prod puppeteer-core
 const puppeteer = require('puppeteer');
-const { puppeteerLogger } = require('../utils');
-const ImdbItem = require('../db/models/ImdbItem');
 
-async function saveItem(itemId) {
-  const item = await ImdbItem.findOne({ id: itemId });
-  if (item) {
-    return item.id;
+const imdbUrl = 'https://www.imdb.com/user/ur60351403/watchlist';
+
+async function getImdbList() {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(imdbUrl);
+    const list = await page.waitForSelector('.lister-list');
+    const lista = await list.$$eval('.lister-item', (nodes) => nodes.map((n) => n.querySelector('.lister-item-header a').innerText));
+    console.log('🚀 ~ file: imdbScrapper.js ~ line 25 ~ lista', lista);
+
+    await browser.close();
+    return lista;
+  } catch (err) {
+    console.error(err);
+    return err;
   }
-  const newItem = new ImdbItem({ id: itemId });
-  await newItem.save();
-  return newItem.id;
-}
-
-async function initPuppeteer() {
-  const browser = await puppeteer.launch();
-  return browser;
-}
-
-async function getImdbList(browser) {
-  const imdbUrl = 'https://www.imdb.com/user/ur60351403/watchlist';
-  // const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(imdbUrl);
-  puppeteerLogger(page);
-  const itemIds = await page.evaluate(async () => {
-    const { items } = window.IMDbReactInitialState[0].list;
-    console.log(items[0]);
-    return items.map((item) => item.const);
-  });
-  console.log(itemIds);
-  const items = await Promise.all(itemIds.map((itemId) => saveItem(itemId)));
-  await browser.close();
-  return items;
 }
 
 async function runImdbScrapper() {
   try {
-    const browser = await initPuppeteer();
-    console.log('🚀 ~ file: imdbScrapper.js ~ line 41 ~ runImdbScrapper ~ browser', browser);
-    const imdbList = await getImdbList(browser);
+    const imdbList = await getImdbList();
     return imdbList;
   } catch (error) {
     return JSON.stringify(error);
