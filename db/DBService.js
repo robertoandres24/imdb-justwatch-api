f8476902b74692034fdc5e85baaf9f3f66a64d0f
roberto <robertoandres24@gmail.com>
2022-05-31 00:23:25 -0400

    sanitize Imdb Title

--- db/DBService.js
+++ db/DBService.js
@@ -1,5 +1,21 @@
 const ImdbItem = require('./models/ImdbItem');
 
+function cleanTitle(title) {
+  return title.replace(/[^a-zA-Z0-9 ]/gm, '')
+    .replace(/ /gm, '-')
+    .toLowerCase();
+}
+/**
+ *
+ * @param {{title: string, details: string}[]} list An array of Imdb Items
+ * @returns same properties, but sanitized
+ */
+function sanitizeList(list) {
+  return list.map((item) => ({
+    details: item.details,
+    title: cleanTitle(item.title),
+  }));
+}
 async function getAll() {
   return ImdbItem.find();
 }
@@ -11,7 +27,8 @@ async function save(item) {
   await newItem.save();
 }
 async function saveImdList(list) {
-  const promisesList = list.map((item) => save(item));
+  const sanitizedList = sanitizeList(list);
+  const promisesList = sanitizedList.map((item) => save(item));
   return Promise.all(promisesList);
 }
 async function removeAll() {
@@ -19,5 +36,5 @@ async function removeAll() {
 }
 
 module.exports = {
-  getAll, findOne, save, saveImdList, removeAll,
+  getAll, findOne, saveImdList, removeAll,
 };
